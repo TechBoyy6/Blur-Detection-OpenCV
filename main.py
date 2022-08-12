c6d0f7e218ed8b8c949636f304c96a199d5eb331
Moiz Rajkotwala <techboyy6@gmail.com>
2022-08-12 11:37:48 +0530

    added basic cv functions

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+import cv2
+import numpy as np
+
+def readImage(path):
+    image = cv2.imread(path)
+    return image
+
+
+def show_images(image):
+    scale_percent = 10
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    cv2.imshow("a", cv2.resize(image, dim, interpolation=cv2.INTER_AREA))
+    cv2.waitKey(0)
