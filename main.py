645bb45e627798e491d813b4d26e2966d4b4fbf7
Moiz Rajkotwala <techboyy6@gmail.com>
2022-08-13 22:39:25 +0530

    image open fixed

--- main.py
+++ main.py
@@ -14,26 +14,27 @@ def readImage(path):
 
 def show_images(image):
     """@brief This function rescales the images and displays it on the screen"""
-    scale_percent = 10
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)  #ye resize proper hai kya ek baar check kar barabar se @moiz
-    dim = (width, height)
+    # scale_percent = 57
+    # width = int(image.shape[1] * scale_percent / 100)
+    # height = int(image.shape[0] * scale_percent / 100)  #ye resize proper hai kya ek baar check kar barabar se @moiz
+    # dim = (width, height)
 
-    cv2.imshow("a", cv2.resize(image, dim, interpolation=cv2.INTER_AREA))
+    cv2.imshow("a", image)
     cv2.waitKey(0)
 
 def method_laplacian(img):
     """@brief Here the Laplacian method is applied and variance of the image is returned
 @param img takes gray scale image"""
     lap_factor = cv2.Laplacian(img, cv2.CV_64F).var()
-    cv2.imshow('Laplacian', lap_factor)
+    # cv2.imshow('Laplacian', lap_factor)
     cv2.waitKey(0)
     print(lap_factor)
     return lap_factor
 
 def main():
-    path = "test1.jpg"
+    path = "D:\college\Blur-Detection-OpenCV\sample\satyam1.jpeg"
     img = readImage(path)
+    show_images(img)
     factor = method_laplacian(img)
     if(factor<blur_factor):
         print("Image to blur")
