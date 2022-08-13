fa78f048aafd634fd6ab906188faa2aa0fd4cd01
Y4t1sh <yashpant200@gmail.com>
2022-08-13 13:52:51 +0530

    check @moiz

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def show_images(image):
     """@brief This function rescales the images and displays it on the screen"""
     scale_percent = 10
     width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)  #ye resize proper hai kya ek baar check kar barabar se @moiz
     dim = (width, height)
 
     cv2.imshow("a", cv2.resize(image, dim, interpolation=cv2.INTER_AREA))
